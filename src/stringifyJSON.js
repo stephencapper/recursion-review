0f7391dbbb3ff864ee30bf4476fa7539e1da01a6
Stephen Capper <stephencapper@gmail.com>
2024-01-22 12:35:54 -0800

    Initial work on stringifyJSON.js

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,33 @@
 
 // but you don't so you're going to write it from scratch:
 
+//Input: primitive values, arrays, objects, functions, (symbols)
+//Output: string (in JSON format)
+//Constraints: don't cheat
+//Edge cases: undefined, empty object, empty array, single values; functions; undefined in an array (null); undefined in an object (ommitted); infinity, NaN (both null);
+
 var stringifyJSON = function(obj) {
-  // your code goes here
+  var resultString = '';
+  //handle the edge cases
+  if (obj === undefined) {
+    return undefined;
+  }
+  if (obj === null || typeof(obj) === 'function' || obj === infinity || obj === NaN) {
+    return null;
+  }
+  if (obj === '') {
+    return '';
+  }
+  //handle primitve values
+  if (typeof(obj) === 'string') {
+    resultString += obj;
+  }
+
+
+  //handle arrays
+
+  //handle objects
+
+  return resultString;
+
 };
